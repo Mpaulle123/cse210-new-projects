860bb7953c6da88f1837168afddb80b5f69e1735
Mpaulle22 <mah22003@byui.edu>
2024-09-24 00:49:05 +0200

    Completed prep 1, 2 and 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 100);
+
+        int userNumber = -1;
+        while ( userNumber != magicNumber)
+        {
+            Console.Write("what is the magic number? ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed the magic number");
+            }
+        
+            else if (userNumber <  magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (userNumber > magicNumber)
+            {
+                Console.WriteLine("lower");
+            }
+        }
     }
 }
